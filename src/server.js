d90d921863d7ab71033d7b8f31a98aed00b958bb
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2021-11-27 22:19:22 +0900

    PullStack_YoutubeCode_getRequest

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening)
