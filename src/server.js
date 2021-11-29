9db56b4f05ddce4dd44f9c569cc47dec00ee26ff
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2021-11-29 21:03:16 +0900

    PullStack_response

--- src/server.js
+++ src/server.js
@@ -6,9 +6,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
-
+const handleHome = (req, res) => {
+    
+    return res.send("I still miss you");
+}
+const handleLogin = (req, res) => {
+    return res.send("Login here!");
+}
 app.get("/", handleHome);
+app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
