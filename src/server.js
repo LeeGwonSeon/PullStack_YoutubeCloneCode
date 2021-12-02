49b1340094ba843ea976e361a133e31821349ebb
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2021-12-02 21:44:14 +0900

    PullStack_Middleware

--- src/server.js
+++ src/server.js
@@ -5,15 +5,19 @@ import express from "express";
 const PORT = 4000;
 
 const app = express();
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
 
 const handleHome = (req, res) => {
     
-    return res.send("I still miss you");
+    return res.send("I'm love middlewares");
 }
 const handleLogin = (req, res) => {
-    return res.send("Login here!");
+    return res.send({ message : "Login here!"});
 }
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
