ef90465e07a23635573e2b99282593ba4e68ad26
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2021-12-27 22:58:54 +0900

    PullStack_RoutherExport

--- src/server.js
+++ src/server.js
@@ -2,30 +2,16 @@
 //Babel is a JavaScript compiler. 사이트 나중에 검색
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./globalRouter";
+import userRouter from "./userRouter";
+import videoRouter from "./videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+
 app.use(logger);
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-const videoRouter = express.Router();
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
