2e49e2da7aa881dd5f8144a5bcc8f9b397b439dd
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2021-12-29 21:47:34 +0900

    PullStack_Controller_Add

---
+++ src/controllers/userController.js
@@ -0,0 +1,5 @@
+
+
+export const join = (req, res) => res.send("Join");
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("remove User");
--- src/globalRouter.js
+++ src/globalRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
+import {join} from "./controllers/userController";
+import {trending} from "./controllers/videoController";
+
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
+globalRouter.get("/", join);
+globalRouter.get("/join", trending);
 
 export default globalRouter;
