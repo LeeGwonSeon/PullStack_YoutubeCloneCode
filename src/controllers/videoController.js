6f382bfa2d8b991f50d64b086868be84b99ad3f3
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-01-17 22:26:39 +0900

    PullStackUpdate_HtmlPug_Extends

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,6 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-    
-    return res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-    
-    return res.send("Edit");
-}
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
