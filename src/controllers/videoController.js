b6038f31a40979b109ebaef93e85f85ca84eff0d
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-01-20 22:20:31 +0900

    PullStack_update_videoArray

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,7 +3,35 @@ const fakeUser = {
     loggedIn: false,
 };
 
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+    const videos = [
+    {
+        title: "First Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:1
+    },
+    {
+        title: "Second Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:1
+    },
+    {
+        title: "Third Video",
+        rating:5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views:59,
+        id:1
+    }
+    ]
+    return res.render("home", {pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
