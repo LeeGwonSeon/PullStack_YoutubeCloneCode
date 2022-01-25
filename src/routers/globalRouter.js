1fc4d2ff3a7bfd414b8b8d5005ffe0c289840d19
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-01-25 22:18:45 +0900

    PullStack_Post_Video_update

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -8,6 +8,5 @@ const globalRouter = express.Router();
 globalRouter.get("/",trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
