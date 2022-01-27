2bc68b2ffeab1bd0ebbc9359b204d19b9a5f2954
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-01-27 22:18:24 +0900

    PullStack_remindRedirect

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -54,6 +54,15 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-    // here we will add a vide to the videos array.
+    const {title} = req.body;
+    const newVideo = {
+        title,
+        rating:0,
+        comments:0,
+        createdAt: "Just now",
+        views:0,
+        id: videos.length + 1,
+    };
+    videos.push(newVideo);
     return res.redirect("/");
 };
