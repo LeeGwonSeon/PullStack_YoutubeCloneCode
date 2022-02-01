0df751be785c76d6a72d4e0633079f140dce8692
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-01 22:09:46 +0900

    PullStack_updateVideoSchema

--- src/db.js
+++ src/db.js
@@ -8,6 +8,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("Connected to DB");
-const handleError = (error) => console.log("DB Error", error)
+const handleError = (error) => console.log("DB Error", error);
+
 db.on("error", handleError);
 db.once("open", handleOpen);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 //Babel is a JavaScript compiler. 사이트 나중에 검색
 import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
