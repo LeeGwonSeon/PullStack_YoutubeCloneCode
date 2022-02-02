7c0cace4de56cb9d170951f906791f4468afcde0
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-02 22:22:19 +0900

    PullStack_update_OurFirstQueryPartTwo

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,9 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => { 
-    Video.find({}, (error, videos) => {});   
-    return res.render("home", {pageTitle: "Home"});
+    Video.find({}, (error, videos) => {
+        return res.render("home", {pageTitle: "Home", videos });
+    }); 
 };
 export const watch = (req, res) => {
     const { id } = req.params;
