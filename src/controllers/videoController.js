e7c47c39baa0b1968bef4bd4948a7cb7e541719a
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-07 20:59:30 +0900

    Creatung a Video part Two_CreateDB&uploadVideoData

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,12 +30,12 @@ export const getUpload = (req, res) => {
     return res.render("upload", {pageTitle:"Upload Video"});
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {title, description, hashtags} = req.body;
     console.log(title, description, hashtags);
-    const video = new Video({
-        title: title,
-        description: description,
+    await Video.create({
+        title,
+        description,
         createdAt: Date.now(),
         hashtags: hashtags.split(",").map((word) => `#${word}`),
         meta: {
@@ -43,6 +43,5 @@ export const postUpload = (req, res) => {
             rating: 0,
         },
     });
-    console.log(video);
     return res.redirect("/");
 };
