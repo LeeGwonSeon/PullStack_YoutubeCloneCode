98b635a4be49c8b411a290f40d265fc1763a3783
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-07 20:28:39 +0900

    Creating a Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,6 +31,18 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-    const {title} = req.body;
+    const {title, description, hashtags} = req.body;
+    console.log(title, description, hashtags);
+    const video = new Video({
+        title: title,
+        description: description,
+        createdAt: Date.now(),
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+        meta: {
+            views: 0,
+            rating: 0,
+        },
+    });
+    console.log(video);
     return res.redirect("/");
 };
