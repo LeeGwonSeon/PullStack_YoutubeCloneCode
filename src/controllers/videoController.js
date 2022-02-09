714efbb856a11d4b6b51bfa3aa55f6d24d809ca8
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-09 21:37:40 +0900

    PullStackUpdate_EditVideopartTwo

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -17,11 +17,18 @@ export const home = async(req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    return res.render("watch", { pageTitle: video.title , video });
-};
-export const getEdit = (req, res) => {
+    if(!video) {
+        return res.render("404" ,{ pageTitle: "Video not found."});
+        }
+        return res.render("watch", { pageTitle: video.title , video });
+    };
+export const getEdit = async(req, res) => {
     const { id } = req.params;
-    return res.render("edit", {pageTitle:`Editing`});
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404" ,{ pageTitle: "Video not found."});
+        }
+    return res.render("edit", {pageTitle:`Edit: ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
