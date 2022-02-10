83213f25703a357600b5437098f59b2c2baa9014
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-10 20:55:58 +0900

    PullStack_EditVideoparTwo

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,9 +30,17 @@ export const getEdit = async(req, res) => {
         }
     return res.render("edit", {pageTitle:`Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+export const postEdit = async(req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404" ,{ pageTitle: "Video not found."});
+        }
+        video.title = title;
+        video.description = description;
+        video.hashtags = hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`);
+        await video.save();
     return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -46,7 +54,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`),
         });
         return res.redirect("/");
     }catch(error){
