7e2c7fa306319ff9bc8f2e8a44ef002a5fb4b417
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-15 20:09:08 +0900

    PullStackUpdate_DeleteVideoDB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -64,3 +64,10 @@ export const postUpload = async (req, res) => {
     }
     
 };
+
+export const deleteVideo = async(req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    // delete video
+    return res.redirect("/");
+}
