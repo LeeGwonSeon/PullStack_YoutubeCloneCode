e7092b16bd6dc137245e698daf661a455258078d
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-22 21:33:59 +0900

    PullStackUpdate_StatusCodes

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,7 +26,7 @@ export const getEdit = async(req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if(!video) {
-        return res.render("404" ,{ pageTitle: "Video not found."});
+        return res.status(404).render("404" ,{ pageTitle: "Video not found."});
         }
     return res.render("edit", {pageTitle:`Edit: ${video.title}`, video });
 };
@@ -35,7 +35,7 @@ export const postEdit = async(req, res) => {
     const {title, description, hashtags} = req.body;
     const video = await Video.exists({ _id: id});
     if(!video) {
-        return res.render("404" ,{ pageTitle: "Video not found."});
+        return res.status(404).render("404" ,{ pageTitle: "Video not found."});
         }
         await Video.findOneAndUpdate(id, {
             title, description, hashtags:Video.formatHashtags(hashtags)
@@ -57,7 +57,7 @@ export const postUpload = async (req, res) => {
         });
         return res.redirect("/");
     }catch(error){
-        return res.render("upload", {
+        return res.status(400).render("upload", {
             pageTitle:"Upload Video",
             errorMessage: error._message,
         });
