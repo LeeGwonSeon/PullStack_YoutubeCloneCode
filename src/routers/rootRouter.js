3d12e2387e22ffec26559dd9d2f2d44d8963fd09
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-22 22:04:54 +0900

    PullStackUpdate_LoginpartOne_MakeLoginPage

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/userController";
+import { getJoin, postJoin, getLogin, postLogin } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 
 
@@ -7,7 +7,7 @@ const rootRouter = express.Router();
 
 rootRouter.get("/",home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
