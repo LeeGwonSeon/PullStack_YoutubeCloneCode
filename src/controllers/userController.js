dc15131b08576f996d500e37741ed1f1c497b5ce
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-02-24 22:19:03 +0900

    PullStackUpdate_LoggedInUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -55,6 +55,8 @@ export const postLogin = async(req, res) => {
             errorMessage:"Wrong password", 
         });
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 };
 
