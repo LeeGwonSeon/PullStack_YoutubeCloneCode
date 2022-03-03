7b08537fcd5b4b695eb2aea570508bb6af9b9052
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-03 23:11:57 +0900

    PullStackUpdate_GithubLoginpartTow

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {edit, remove, logout, see}from "../controllers/userController";
+import {edit, remove, logout, see, startGithubLogin, finishGithunLogin}from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithunLogin);
 userRouter.get(":id", see);
 
 
