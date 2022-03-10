f50208ffb5afa6758cb7f0d635967c58eeb5f093
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-10 22:56:13 +0900

    PullStackUpdate_GithubLoginpartThreeRecovery_20220310

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -74,13 +74,23 @@ export const startGithubLogin = (req, res) => {
     return res.redirect(finalURL);
 };
 
-export const finishGithubLogin = (req, res) => {
+export const finishGithubLogin = async (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
     const config = {
         client_id: process.env.GH_CLIENT,
         client_secret: process.env.GH_SECRET,
         code: req.query.code,
     };
-    console.log(config);
+    const params = new URLSearchParams(config).toString();
+    const finalURL = `${baseUrl}?${params}`;
+    const data = await fetch(finalURL,{
+        method:"POST",
+        headers: {
+            Accept: "application/json",
+        }
+    })
+    const json = await data.json();
+    console.log(json);
 };
 
 export const edit = (req, res) => res.send("Edit User");
