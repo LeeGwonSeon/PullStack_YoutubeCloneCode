90ca3e31807dd6bb8a7d3ed3fc62ea9037be5885
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-21 20:37:14 +0900

    PullStackUpdate_Protector&PublicMiddlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,4 +3,21 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
     next();
-}
+};
+
+
+export const protectorMiddleware = (req, res, next) => {
+    if(req.session.loggedIn){
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn){
+        return next();
+    } else {
+        return res.redirect("/");
+    }
+};
