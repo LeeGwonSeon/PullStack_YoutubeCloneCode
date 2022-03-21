05e4d28d2ec5f858177d2a5bc2f0f377db914a51
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-21 20:16:12 +0900

    PullStackUpdate_EditProfileGET

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -143,5 +143,11 @@ export const logout = (req, res) => {
     req.session.destroy();
     return res.redirect("/");
 };
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", { pageTitle: "Edit Profile" });
+}
+export const postEdit = (req, res) => {
+    return res.render("edit-profile");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {edit, logout, see, startGithubLogin, finishGithubLogin}from "../controllers/userController";
+import {getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin}from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
