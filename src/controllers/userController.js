132a04fed4f0cc0b8d241859a2204bb8a8ba09a6
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-22 21:56:31 +0900

    PullStackUpdate_EditProfilePOSTpartOne

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -146,7 +146,20 @@ export const logout = (req, res) => {
 export const getEdit = (req, res) => {
     return res.render("edit-profile", { pageTitle: "Edit Profile" });
 }
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
+    const {
+        session: {
+            user: { _id },
+        },
+        body: {name, email, username, location},
+    } = req;
+
+    await User.findByIdAndUpdate(_id, {
+        name, 
+        email, 
+        username, 
+        location,
+    });
     return res.render("edit-profile");
 };
 export const edit = (req, res) => res.send("Edit User");
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,7 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
+    console.log(req.session.user);
     next();
 };
 
