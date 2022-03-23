a29ce1fefd01a184defc3a8364a963fb222027e4
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-23 20:55:23 +0900

    PullStackUpdate_ChangePassword part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -164,4 +164,16 @@ export const postEdit = async (req, res) => {
     req.session.user = updatedUser;
     return res.redirect("/users/edit");
 };
+
+export const getChangePassword = (req, res) => {
+    if (req.session.user.socialOnly === true) {
+        return res.redirect("/");
+    }
+    return res.render("users/change-password", { pageTitle:"Change Password" });
+};
+export const postChangePassword = (req, res) => {
+    //send notification
+    return res.redirect("/");
+}
+
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin}from "../controllers/userController";
+import {getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword}from "../controllers/userController";
 import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
