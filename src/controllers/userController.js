c318e8fd8a5f0b3b6a0270b9a06de5edcd70e1e8
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-23 21:51:23 +0900

    PullStackUpdate_Change Password part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -171,9 +171,30 @@ export const getChangePassword = (req, res) => {
     }
     return res.render("users/change-password", { pageTitle:"Change Password" });
 };
-export const postChangePassword = (req, res) => {
-    //send notification
-    return res.redirect("/");
-}
+export const postChangePassword = async (req, res) => {
+    const {session: {
+        user: { _id},
+    },
+        body: {oldPassword, newPassword, newPasswordConfirmation},
+    } = req;
+    const user = await User.findById(_id);
+    const ok = await bcrypt.compare(oldPassword, user.password);
+    if(!ok){
+        return res.status(400).render("users/change-password", {
+            pageTitle: "Change Password",
+            errorMessage: "The current password is incorrect", });
+    }
+    if(newPassword !== newPasswordConfirmation){
+        return res.status(400).render("users/change-password", {
+            pageTitle: "Change Password",
+            errorMessage: "The password dose not match the confirmation", });
+    }
+    
+    console.log("Old password", user.password);
+    user.password = newPassword;
+    console.log("New unhashed pw", user.password);
+    await user.save();
+    return res.redirect("/users/logout");
+};
 
 export const see = (req, res) => res.send("See User");
