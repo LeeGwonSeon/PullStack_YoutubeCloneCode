f7afa204a1a75ff99940e634225bb8f770c0c257
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-28 21:27:51 +0900

    PullStackUpdate_Video Upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -47,12 +47,14 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+    const {path: fileUrl} = req.file;
     const {title, description, hashtags} = req.body;
     console.log(title, description, hashtags);
     try{
         await Video.create({
             title,
             description,
+            fileUrl,
             hashtags:Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {getEdit, postEdit, logout, see, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword}from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles, } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware, avatarUpload } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
