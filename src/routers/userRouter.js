9ec8820801d0a27790d95be62ad6035726e2fdb9
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-29 21:22:24 +0900

    PullStackUpdate_User Profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,6 +9,6 @@ userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUploa
 userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 
 export default userRouter;
