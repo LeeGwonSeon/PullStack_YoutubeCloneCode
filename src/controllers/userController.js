bc6490d24a5252f4721f4b6d961787fe072c2b0d
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-30 21:09:52 +0900

    PullStackUpdate_Video Owner part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import Video from "../models/Video";
 
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -200,6 +201,10 @@ export const see = async(req, res) => {
     if(!user) {
         return res.status(404).render("404", {pageTitle: "User not found." });
     }
-    return res.render("users/profile", { pageTitle: user.name,
-         user });
+    const videos = await Video.find({ owner: user._id });
+    return res.render("users/profile", { 
+        pageTitle: user.name,
+        user,
+        videos,
+     });
 };
