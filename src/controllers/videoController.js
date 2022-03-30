6aeec73093ec73293c54298898092af211ab9d35
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-03-30 21:42:04 +0900

    PullStackUpdate_User's Videos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -52,13 +52,17 @@ export const postUpload = async (req, res) => {
     const {path: fileUrl} = req.file;
     const {title, description, hashtags} = req.body;
     try{
-        await Video.create({
+        const newVideo = await Video.create({
             title,
             description,
             fileUrl,
             owner: _id,
             hashtags:Video.formatHashtags(hashtags),
         });
+        const user = await User.findById(_id);
+        user.videos.push(newVideo._id);
+        user.save();
+
         return res.redirect("/");
     }catch(error){
         return res.status(400).render("upload", {
--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String },
     location: String,
+    videos: [{ type: mongoose.Schema.Types.ObjectId, ref:"Video"}],
 });
 
 userSchema.pre('save', async function(){
