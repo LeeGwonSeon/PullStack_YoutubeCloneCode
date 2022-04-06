0345a565ba399cdf3931756ccaa21a2ab899c092
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-06 21:53:43 +0900

    PullStackUpdate_MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
     entry: "./src/client/js/main.js",
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+    })],
     mode: "development",
     output:{
-        filename: "main.js",
-        path: path.resolve(__dirname, "assets", "js"),
+        filename: "js/main.js",
+        path: path.resolve(__dirname, "assets"),
     },
     module: {
         rules: [
@@ -21,7 +25,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
