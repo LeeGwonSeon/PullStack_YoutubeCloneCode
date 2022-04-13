b4eaaee9e682f418ee6342f89ef84ed06ab2c44f
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-14 00:17:01 +0900

    PullStackUpdate_Play Pause error_can't find videoPlayer connect

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,10 @@
-console.log("video player");
+const video = document.querySelector("video");
+const play = document.getElementById("play");
+const mute = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+console.log(play
+    ,mute
+    ,time
+    ,volume);
