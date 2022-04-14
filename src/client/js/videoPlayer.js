2c3600a951824619dc065bda4bed8dcc19669616
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-14 21:42:41 +0900

    PullStackUpdate_Volume complete Real

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,10 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
     // if the video is playing, pause it
     if (video.paused){
@@ -24,8 +28,21 @@ const handleMute = (e) => {
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
 };
+
+const handleVolumeChange = (event) => {
+    const {
+        target: { value },
+    } = event;
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolumeChange);
