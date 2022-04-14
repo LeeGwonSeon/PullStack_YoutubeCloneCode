e05d0d1427815ae5e14e27907ec013ff88d90812
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-14 21:18:24 +0900

    PullStackUpdate_Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
     // if the video is playing, pause it
@@ -11,15 +11,21 @@ const handlePlayClick = (e) => {
     } else {
         video.pause();
     }
-    // else play the video.
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
 const handlePause = () => (playBtn.innerText = "Play");
 const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {};
+const handleMute = (e) => {
+    if(video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
