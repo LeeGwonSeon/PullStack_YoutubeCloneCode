5035ec8c27cd21f87dd8911404e0e6e25f8a82b0
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-18 21:15:01 +0900

    PullStackUpdate_Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 
 let volumeValue = 0.5;
@@ -43,6 +44,16 @@ const handleVolumeChange = (event) => {
     video.volume = value;
 }
 
+const handleLoadedMetadata = () => {
+    totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+    currentTime.innerText = Math.floor(video.currentTime);
+}
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
