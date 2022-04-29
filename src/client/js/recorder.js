f70f3392af7c93205b3a226388a47bb07726c7fe
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-29 09:25:34 +0900

    PullStackUpdate_Downloading the File

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,9 +3,14 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
 const handleDownload = () => {
-
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
 }
 
 const handleStop = () => {
@@ -21,9 +26,9 @@ const handleStart = () => {
     startBtn.innerText = "Stop Recording";
     startBtn.removeEventListener("click", handleStart);
     startBtn.addEventListener("click", handleStop);
-    recorder = new MediaRecorder(stream);
+    recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
-        const videoFile = URL.createObjectURL(event.data);
+        videoFile = URL.createObjectURL(event.data);
         video.srcObject = null;
         video.src = videoFile;
         video.loop = true;
