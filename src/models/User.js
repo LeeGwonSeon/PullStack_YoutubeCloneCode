4e4613ccc425ba920806b012522a060880c00943
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-05-17 20:36:58 +0900

    Comment Models

--- src/models/User.js
+++ src/models/User.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String },
     location: String,
+    comments: [ { type: mongoose.Schema.Types.ObjectId, ref:"Comment"},
+    ],
     videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
