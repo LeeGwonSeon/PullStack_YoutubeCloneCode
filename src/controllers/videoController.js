f35229cd61fc501cf701d7666261eca292b1f29f
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-05-18 21:10:03 +0900

    PullStackUpdate_API Route part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -124,4 +124,8 @@ export const registerView = async(req, res) => {
     video.meta.views = video.meta.views + 1;
     await video.save();
     return res.sendStatus(200);
+};
+
+export const createComment = (req, res) => {
+    return res.end();
 }
