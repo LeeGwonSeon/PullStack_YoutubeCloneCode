bbfc86ff4721e0e5264708d0d18633a1645fd00f
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-05-19 21:16:45 +0900

    PullStackUpdate_Commenting

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -4,15 +4,16 @@ const form = document.getElementById("commentForm");
 const handleSubmit = (event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
-    const text = textarea.value;
+    let text = textarea.value;
     const videoId = videoContainer.dataset.id;
     if (text === ""){
         return;
     }
     fetch(`/api/videos/${videoId}/comment`,{
         method: "POST",
-        body: JSON.stringify({ text }),
+        body: JSON.stringify({text}),
     });
+    textarea.value = "";
 };
 
 if(form){
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
-import User from "../models/User";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
+import User from "../models/User";
 /*
 console.log("start")
 Video.find({}, (error, videos) => {
@@ -126,9 +127,20 @@ export const registerView = async(req, res) => {
     return res.sendStatus(200);
 };
 
-export const createComment = (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
-    console.log(req.body.text, req.body.rating);
-    return res.end();
+export const createComment = async (req, res) => {
+    const {
+      session: {user},
+      body: {text},
+      params: {id},
+    } = req;
+    const video = await Video.findById(id);
+    if (!video) {
+      return res.sendStatus(404);
+    }
+    const comment = await Comment.create({
+      text,
+      owner: user._id,
+      video: id,
+    });
+    return res.sendStatus(201);
 }
