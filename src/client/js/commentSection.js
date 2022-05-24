22420f2c981868bb8f308068d40a766d3d0e3d8c
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-05-24 22:28:22 +0900

    PullStackUpdate_Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,6 +3,19 @@ import { async } from "regenerator-runtime";
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.appendChild(newComment);
+}
+
 const handleSubmit = async(event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
@@ -11,11 +24,14 @@ const handleSubmit = async(event) => {
     if (text === ""){
         return;
     }
-    await fetch(`/api/videos/${videoId}/comment`,{
+    const {status} = await fetch(`/api/videos/${videoId}/comment`,{
         method: "POST",
         body: JSON.stringify({text}),
     });
     textarea.value = "";
+    if (status === 201) {
+        addComment(text);
+    }
 };
 
 if(form){
